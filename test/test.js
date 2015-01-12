b5d46472d8a3e63bbb5af5148788d2165745a42b
Julian Gruber <julian@juliangruber.com>
2015-01-12 15:32:00 +0100

    refactor tests to rely on less global state

--- test/test.js
+++ test/test.js
@@ -1,19 +1,14 @@
 var ok = require('assert').ok
+var equal = require('assert').equal
 var Cache = require('..')
 var fs = require('fs')
 var concat = require('concat-stream')
-
-var hit = false
 
 var redisClient = function() {
   var cache = {}
   return {
     get: function(key, cb) {
-      hit = false
-      if (cache[key]) {
-        hit = true
-        return cb(null, cache[key])
-      }
+      if (cache[key]) return cb(null, cache[key])
       cb(new Error('Not found'))
     },
     set: function(key, val, cb) {
@@ -33,40 +28,69 @@ var getSlowStream = function() {
   return fs.createReadStream(__filename)
 }
 
-var opts = {
-  cache: redisClient,
-  key: 'abc',
-  ttl: 100,
-  get: getSlowStream
-}
-
-it('should getStream', function(done) {
-  Cache(opts).pipe(concat(function(a) {
+it('should create stream on cache miss', function(done) {
+  var miss = false
+  Cache({
+    cache: redisClient,
+    key: String(Math.random()),
+    ttl: 100,
+    get: function() {
+      miss = true
+      return getSlowStream()
+    }
+  }).pipe(concat(function(a) {
     getSlowStream().pipe(concat(function(b) {
-      ok(a.toString() === b.toString(), 'wrong value')
+      ok(a.toString() === b.toString(), 'same value')
+      ok(miss, 'cache miss')
       done()
     }))
   }))
 })
 
-it('should getStream from cache', function(done) {
+it('should read cache on cache hit', function(done) {
+  var key = String(Math.random())
+  var miss = false
+  var opts = {
+    cache: redisClient,
+    key: key,
+    ttl: 100,
+    get: function() {
+      miss = true
+      return fs.createReadStream(__filename)
+    }
+  }
   Cache(opts).pipe(concat(function(a) {
-    getSlowStream().pipe(concat(function(b) {
-      ok(a.toString() === b.toString(), 'wrong value')
-      ok(hit === true, 'not from cache')
+    equal(miss, true, 'cache miss')
+    miss = false
+    Cache(opts).pipe(concat(function(b) {
+      equal(miss, false, 'cache hit')
+      equal(a.toString(), b.toString(), 'same value')
       done()
     }))
   }))
 })
 
 it('should getStream expired cache', function(done) {
-  setTimeout(function() {
-    Cache(opts).pipe(concat(function(a) {
-      getSlowStream().pipe(concat(function(b) {
-        ok(a.toString() === b.toString(), 'wrong value')
-        ok(hit === false, 'should not cache hit')
+  var key = String(Math.random())
+  var miss = false
+  var opts = {
+    cache: redisClient,
+    key: key,
+    ttl: 100,
+    get: function() {
+      miss = true
+      return fs.createReadStream(__filename)
+    }
+  }
+  Cache(opts).pipe(concat(function(a) {
+    equal(miss, true, 'cache miss')
+    miss = false
+    setTimeout(function() {
+      Cache(opts).pipe(concat(function(b) {
+        equal(miss, true, 'cache miss after expiry')
+        equal(a.toString(), b.toString(), 'same value')
         done()
       }))
-    }))
-  }, 200)
+    }, 200)
+  }))
 })
